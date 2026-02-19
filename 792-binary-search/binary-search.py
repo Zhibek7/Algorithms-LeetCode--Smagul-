59123fcb48af94a336ef55c3482cb1032822c993
Zhibek7 <smagulzhibek@gmail.com>
2026-02-19 21:54:17 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (57.38%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -9,5 +9,5 @@ class Solution:
             elif nums[mid] < target:
                 left = mid + 1
             else:
-                right = mid - 1
+                right = mid -1
         return -1
