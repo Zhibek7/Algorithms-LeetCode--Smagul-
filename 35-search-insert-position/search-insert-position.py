7f668ed6da7bd2ec33751a8f07e73621712364df
Zhibek7 <smagulzhibek@gmail.com>
2026-02-19 20:35:20 +0500

    Time: 0 ms (100.00%) | Memory: 19.9 MB (62.61%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -11,5 +11,4 @@ class Solution:
                 left = mid +1
             else:
                 right = mid -1
-        
         return left # По завершению цикла left указывает на индекс для вставки
