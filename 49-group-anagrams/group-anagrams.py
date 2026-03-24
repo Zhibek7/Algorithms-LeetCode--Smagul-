e6142f54fb0a4ee5f08fca9750c49f8a04d8f92b
Zhibek7 <smagulzhibek@gmail.com>
2026-03-25 02:07:01 +0500

    Time: 15 ms (45.42%) | Memory: 22 MB (74.99%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Инициализация хэш-таблицы для группировки по инвариантному ключу
+        anagram_map = {}
+        
+        for s in strs:
+            # Канонизация: сортировка символов создает общий ключ для всех анаграмм
+            key = "".join(sorted(s)) 
+            
+            # Если хэш-функция не находит ключ, создаем новый бакет (bucket)
+            if key not in anagram_map:
+                anagram_map[key] = []
+            
+            # Добавление оригинальной строки в список по вычисленному хэш-адресу
+            anagram_map[key].append(s)
+            
+        # Возврат агрегированных данных (значений хэш-таблицы)
+        return list(anagram_map.values())
